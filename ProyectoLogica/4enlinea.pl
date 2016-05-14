a3047c0f50ed4b1a05fa86bffa20c208a81879d5
Christian <Christian@DESKTOP-GHF131J>
2016-05-14 01:59:35 -0300

    Casi terminado cuatro, falta inteligencia

--- ProyectoLogica/4enlinea.pl
+++ ProyectoLogica/4enlinea.pl
@@ -8,24 +8,28 @@ en([X,Y], [ [p(X,Y,R)| Rs] | Xs], R).
 
 % cuatro en linea(+Color, +Conf, -CuatroEnLinea) 
 
-cuatro(Color, Conf, Resultado):- cuatroAux(Color, Conf, 0, Aux), longitud(Aux,0,Rictor), mostrar4(Aux, Resultado,Rictor).
-cuatroAux(Color, Conf, Contador, Resultado):- cuatroH(Color, Conf, Contador, Resultado, Conf).
+cuatro(Color, Conf, Resultado):- cuatroAux(Color, Conf, 0, Aux), longitud(Aux,0,Long), mostrar4(Aux, Resultado,Long);
+								 trans(Conf, Traspuesta), cuatroH(Color, Traspuesta, 0, Resultado), mostrar4(Aux, Resultado,Long);
+								 ced(Color,Conf,Resultado).
+cuatroAux(Color, Conf, Contador, Resultado):- cuatroH(Color, Conf, Contador, Resultado).
 
-cuatroH(Color, [[]|Ys], Contador, Resultado, Conf2) :- cuatroH(Color, Ys, 0, Resultado, Conf2).
-cuatroH(Color, Conf, 4, [], Conf2).
-cuatroH(Color, [[p(X,Y,Color)|Xs]|Ys], Contador, [p(X,Y,Color)|Resultado], Conf2):- Contador1 is Contador+1, cuatroH(Color, [Xs|Ys], Contador1, Resultado, Conf2).
-cuatroH(Color, [[p(X,Y,R)|Xs]|Ys], Contador, Resultado,Conf2):- cuatroH(Color, [Xs|Ys], 0, Resultado, Conf2).
+%Cuatro Horizontal (utilizado para vertical)
 
-cuatroH(Color, [], Contador, Vertical, Conf2) :- trans(Conf2, Traspuesta), cuatroV(Color, Traspuesta, 0, Vertical, Conf2).
+cuatroH(Color, [[]|Ys], Contador, Resultado) :- cuatroH(Color, Ys, 0, Resultado).
+cuatroH(Color, Conf, 4, []).
+cuatroH(Color, [[p(X,Y,Color)|Xs]|Ys], Contador, [p(X,Y,Color)|Resultado]):- Contador1 is Contador+1, cuatroH(Color, [Xs|Ys], Contador1, Resultado).
+cuatroH(Color, [[p(X,Y,R)|Xs]|Ys], Contador, Resultado):- cuatroH(Color, [Xs|Ys], 0, Resultado).
 
-cuatroV(Color, [[]|Ys], Contador, Resultado, Conf2) :- cuatroV(Color, Ys, 0, Resultado, Conf2).
-cuatroV(Color, Conf, 4, [], Conf2).
-cuatroV(Color, [[p(X,Y,Color)|Xs]|Ys], Contador, [p(X,Y,Color)|Resultado], Conf2):- Contador1 is Contador+1, cuatroV(Color, [Xs|Ys], Contador1, Resultado, Conf2).
-cuatroV(Color, [[p(X,Y,R)|Xs]|Ys], Contador, Resultado,Conf2):- cuatroV(Color, [Xs|Ys], 0, Resultado, Conf2).
+%Cuatro diagonal
 
-cuatroV(Color, [], Contador, Diagonal, Conf2) :- CuatroD(Color, Conf2, 0, Diagonal, Conf2).
+ced(Color,[Lista | [ Lista2 | Lista3 ] ],Resultado):- cedRecorrerFila(Color,[Lista | [ Lista2 | Lista3 ] ],1,Resultado);cedRecorrerFila(Color,[ Lista2 | Lista3 ],2,Resultado);cedRecorrerFila(Color,Lista3,3,Resultado).
 
-cuatroD(Color, [[p(X,Y,Color)|Xs]|Ys], Contador, Diagonal, Conf2).
+cedRecorrerFila(Color,[PrimeraFila | RestoFilas],X,Res):- cedFila(0,Color,PrimeraFila,X,Y,RestoFilas),generar(0,Color,X,Y,Res).
+
+cedFila(4,Color,Fila,PosActual,ColActual,FilasSiguientes).
+cedFila(Cont,Color, Fila, PosActual, ColActual, [ProxFila | FilasSiguientes]):- buscar(PosActual,ColActual,Fila,Color), Cont1 is Cont+1, Pos is PosActual+1, Col is ColActual+1,cedFila(Cont1,Color,ProxFila,Pos,Col,FilasSiguientes).
+
+
 
 % metodos auxiliares
 
@@ -33,6 +37,15 @@ cuatroD(Color, [[p(X,Y,Color)|Xs]|Ys], Contador, Diagonal, Conf2).
 
 mostrar4([_|Xs], Zs, Cont):- Cont1 is Cont-1, mostrar4(Xs, Zs, Cont1).
 mostrar4(Zs, Zs, 4).
+
+%Busca en una lista la posicion cuyas coordenadas son X,Y y permite obtener su color actual.
+buscar(X,Y,[p(X,Y,Color) | RestoFila],Color).
+buscar(X,Y,[p(W,Z,C) | RestoFila],Color):- buscar(X,Y,RestoFila,Color).
+
+%Genera una lista con la diagonal ganadora
+
+generar(4,Color,X,Y,Res).
+generar(Cont,Color,X,Y,[p(X,Y,Color)| Res]):- Cont1 is Cont+1, X1 is X+1, Y1 is Y+1, generar(Cont1,Color,X1,Y1,Res).
 
 % Obtiene la longitud de una lista.
 
@@ -62,15 +75,3 @@ colocar_ficha(Color, [X, Y] , [ [p(X, Y, vacio)|Ys] | Xs ], [ p(X,Y,Color) | Zs 
 colocar_ficha(Color, [X, Y] , [ [p(Z, W, C)|Ys] | Xs ], [ p(Z,W,C) | Zs ]) :- colocar_ficha(Color, [X,Y], [Ys|Xs], Zs).
 
 % jugada maquina(+Color, +Conf, -Ranura)
-
-
-
-
-
-
-
-
-
-
-
-
