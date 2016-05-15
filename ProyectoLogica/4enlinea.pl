482d6b90b5427d9a4723404bd00b2a92334fd029
Tomas <Tomas@TOMACO>
2016-05-14 21:03:45 -0300

    *Inteligencia, falta Jugada Mate

--- ProyectoLogica/4enlinea.pl
+++ ProyectoLogica/4enlinea.pl
@@ -1,4 +1,5 @@
 % en(+p, +Conf, -Contenido)
+% Retorna el contenido de la ranura pasada por parámetro
 
 en([X, Y], [ [p(Z,W,R)|Rs] | Xs], Res) :- en([X,Y], [Rs|Xs], Res).
 en([X, Y], [ [p(Z,W,R)|[]] | Xs], Res) :- en([X,Y], Xs, Res).
@@ -6,17 +7,16 @@ en([X,Y], [ [p(X,Y,R)| Rs] | Xs], R).
 
 % Ej. conf = [[p(4,1,a), p(4,2,a), p(4,3,a), p(4,4,a)],[p(3,1,a), p(3,2,a), p(3,3,a), p(3,4,a)],[p(2,1,a), p(2,2,a), p(2,3,a), p(2,4,a)],[p(1,1,a), p(1,2,a), p(1,3,a), p(1,4,a)]]
 
-% cuatro en linea(+Color, +Conf, -CuatroEnLinea) 
+% cuatro(+Color, +Conf, -CuatroEnLinea) 
+% Si es true retorna la lista que conforma cuatro en linea, si es false retorna false.
 
 cuatro(Color, Conf, Resultado):- 
 
- cuatroAux(Color, Conf, 0, Aux), longitud(Aux,0,Long), mostrar4(Aux, Resultado,Long);
- trans(Conf, Traspuesta), cuatroH(Color, Traspuesta, 0, Aux), longitud(Aux,0,Long), mostrar4(Aux, Resultado,Long);
- ced(Color,Conf,Aux),longitud(Aux,0,Long),mostrar4(Aux,Resultado,Long).
+ cuatroH(Color, Conf, 0, Aux), longitud(Aux,0,Long), mostrar(4,Aux, Resultado,Long);
+ trans(Conf, Traspuesta), cuatroH(Color, Traspuesta, 0, Aux), longitud(Aux,0,Long), mostrar(4,Aux, Resultado,Long);
+ ced(Color,Conf,Aux),longitud(Aux,0,Long),mostrar(4,Aux,Resultado,Long).
 
-	
-cuatroAux(Color, Conf, Contador, Resultado):- cuatroH(Color, Conf, Contador, Resultado).
-
+ 
 %Cuatro Horizontal (utilizado para vertical)
 
 cuatroH(Color, [[]|Ys], Contador, Resultado) :- cuatroH(Color, Ys, 0, Resultado).
@@ -32,14 +32,14 @@ ced(Color,[Lista | [ Lista2 | Lista3 ] ],Resultado):-
 	cedRecorrerFila(Color,Lista3,4,Resultado).
 
 cedRecorrerFila(Color,[PrimeraFila | RestoFilas],X,Res):- 
-	cedFila(0,Color,PrimeraFila,X,1,RestoFilas),write("entro uno"),generar(0,Color,X,1,Res);
-	cedFila(0,Color,PrimeraFila,X,2,RestoFilas),write("entro dos"),generar(0,Color,X,2,Res);
-	cedFila(0,Color,PrimeraFila,X,3,RestoFilas),write("entro tres"),generar(0,Color,X,3,Res);
-	cedFila(0,Color,PrimeraFila,X,4,RestoFilas),write("entro cuatro"),generar(0,Color,X,4,Res);
-	cedFilaD(0,Color,PrimeraFila,X,4,RestoFilas),write("entro uno"),generarD(0,Color,X,4,Res);
-	cedFilaD(0,Color,PrimeraFila,X,5,RestoFilas),write("entro dos"),generarD(0,Color,X,5,Res);
-	cedFilaD(0,Color,PrimeraFila,X,6,RestoFilas),write("entro seis seis"),generarD(0,Color,X,6,Res);
-	cedFilaD(0,Color,PrimeraFila,X,7,RestoFilas),write("entro cuatro"),generarD(0,Color,X,7,Res).
+	cedFila(0,Color,PrimeraFila,X,1,RestoFilas), generar(0,Color,X,1,Res);
+	cedFila(0,Color,PrimeraFila,X,2,RestoFilas), generar(0,Color,X,2,Res);
+	cedFila(0,Color,PrimeraFila,X,3,RestoFilas), generar(0,Color,X,3,Res);
+	cedFila(0,Color,PrimeraFila,X,4,RestoFilas), generar(0,Color,X,4,Res);
+	cedFilaD(0,Color,PrimeraFila,X,4,RestoFilas),generarD(0,Color,X,4,Res);
+	cedFilaD(0,Color,PrimeraFila,X,5,RestoFilas),generarD(0,Color,X,5,Res);
+	cedFilaD(0,Color,PrimeraFila,X,6,RestoFilas),generarD(0,Color,X,6,Res);
+	cedFilaD(0,Color,PrimeraFila,X,7,RestoFilas),generarD(0,Color,X,7,Res).
 
 % Diagonal ID
 
@@ -59,15 +59,32 @@ cedFilaD(Cont,Color, Fila, PosActual, ColActual, [ProxFila | FilasSiguientes]):-
 cedFilaD(Cont,Color, Fila, PosActual, ColActual, []):- 
 	buscar(PosActual,ColActual,Fila,Color), Cont1 is Cont+1, Pos is PosActual-1, Col is ColActual-1,cedFilaD(Cont1,Color,ProxFila,Pos,Col,FilasSiguientes).
 
-% Metodos auxiliares
+% % % % % % % % % % % %
+% Metodos auxiliares  % 
+% %  % % % % % %  % % %
 
+% tres(+Color, +Conf, Resultado)
+% Si es true retorna la lista que conforma tres en linea, si es false retorna false.
+
+tres(Color, Conf, Resultado):- cuatroH(Color, Conf, 1, Aux), longitud(Aux,0,Long), mostrar(3,Aux, Resultado,Long);
+ trans(Conf, Traspuesta), cuatroH(Color, Traspuesta, 1, Aux), longitud(Aux,0,Long), mostrar(3,Aux, Resultado,Long);
+ ced(Color,Conf,Aux),longitud(Aux,0,Long),mostrar(3,Aux,Resultado,Long).
+ 
+% dos(+Color, +Conf, Resultado)
+% Si es true retorna la lista que conforma dos en linea, si es false retorna false.
+
+ dos(Color, Conf, Resultado):- cuatroH(Color, Conf, 2, Aux), longitud(Aux,0,Long), mostrar(2,Aux, Resultado,Long);
+ trans(Conf, Traspuesta), cuatroH(Color, Traspuesta, 2, Aux), longitud(Aux,0,Long), mostrar(2,Aux, Resultado,Long);
+ ced(Color,Conf,Aux),longitud(Aux,0,Long),mostrar(2,Aux,Resultado,Long).
+
+% generar() COMPLETAR
 generarD(4,Color,X,Y,Res).
 generarD(Cont,Color,X,Y,[p(X,Y,Color)| Res]):- Cont1 is Cont+1, X1 is X-1, Y1 is Y-1,generarD(Cont1,Color,X1,Y1,Res).
 	
-% Muestra los ultimos 4 elementos de una lista
+% mostrar(+N). Muestra los ultimos n elementos de una lista
 
-mostrar4([_|Xs], Zs, Cont):- Cont1 is Cont-1, mostrar4(Xs, Zs, Cont1).
-mostrar4(Zs, Zs, 4).
+mostrar(N,[_|Xs], Zs, Cont):- Cont1 is Cont-1, mostrar(N,Xs, Zs, Cont1).
+mostrar(N, Zs, Zs, N).
 
 %Busca en una lista la posicion cuyas coordenadas son X,Y y permite obtener su color actual.
 buscar(X,Y,[p(X,Y,Color) | RestoFila],Color).
@@ -111,10 +128,15 @@ colocarAux(Color, [X,Y], [p(Z,W, C)| XXs], [p(Z,W,C)|Zs]):- colocarAux(Color, [X
 jugada_maquina(Color, Conf, Ranura) :- jugada_ganadora(Color, Conf, R, Ranura);jugada_bloqueo(Color, Conf, Ranura).
 
 jugada_ganadora(Color, Conf, [X,Y], [X,Y]) :- colocar_ficha(Color, [X,Y], Conf, ConfRes), cuatro(Color, ConfRes, Cuatro).
-%jugada_ganadora(Color, Conf, [X,Y], Resultado) :- X>0, Y<8,Y1 is Y+1,  jugada_ganadora(Color, Conf, [X, Y1], Resultado).
-%jugada_ganadora(Color, Conf, [X,Y], Resultado) :- Y=8, X1 is X-1, jugada_ganadora(Color, Conf,[X1,1], Resultado).
 jugada_bloqueo(Color,Conf, Resultado) :- Color=a, jugada_ganadora(r, Conf, Ranura, Resultado).
 jugada_bloqueo(Color,Conf, Resultado) :- Color=r, jugada_ganadora(a, Conf, Ranura, Resultado).
-%jugada_segura(Color, Conf, Resultado) :- 
+
+jugada_segura(Color, Conf, R) :- jugada_inicial(Color, Conf, R).
+jugada_segura(Color, Conf, R) :- colocar_ficha(Color, R, Conf, ConfRes), tres(Color, ConfRes, Tres).
+jugada_segura(Color, Conf, R) :- colocar_ficha(Color, R, Conf, ConfRes), dos(Color, ConfRes, Dos).
+jugada_inicial(Color, Conf, [6,4]) :- disponible([6,4], Conf).
+
+%jugada_mate(Color, )
+
 
 
